af8006ea89a70eb29fb52147c165f901036bca32
Sebastián Estrella <sestrella.me@gmail.com>
2019-07-09 12:34:27 -0500

    Complete first code snippet

--- src/main/java/com/stackbuilders/App.java
+++ src/main/java/com/stackbuilders/App.java
@@ -6,22 +6,30 @@ public class App {
   public static void main(String[] args) {
     User[] users = new User[] {
       // admins
-      new User("Bruce", "Banner", true),
-      new User("Clinton", "Barton", true),
-      new User("Natasha", "Romanoff", true),
-      new User("Steve", "Rogers", true),
-      new User("Thor", "Odisen", true),
-      new User("Tony", "Stark", true),
+      new User("Bruce", "Banner", true, true),
+      new User("Clinton", "Barton", true, false),
+      new User("Natasha", "Romanoff", true, true),
+      new User("Steve", "Rogers", true, true),
+      new User("Thor", "Odisen", true, true),
+      new User("Tony", "Stark", true, true),
       // members
-      new User("Peter", "Parker", false),
-      new User("Stephen", "Strange", false)
+      new User("Peter", "Parker", false, false),
+      new User("Stephen", "Strange", false, true)
     };
 
     Table table = new Table();
 
     for (User user : users) {
       if (user.isEnabled()) {
-
+        Row row = new Row();
+        row.addColumn(new Column(user.getFirstName() + " " + user.getLastName()));
+        String role = "";
+        if (user.isAdmin()) {
+          role = "ADMIN";
+        } else {
+          role = "MEMBER";
+        }
+        table.addRow(row);
       }
     }
 
--- src/main/java/com/stackbuilders/User.java
+++ src/main/java/com/stackbuilders/User.java
@@ -3,11 +3,13 @@ package com.stackbuilders;
 public class User {
   private final String firstName;
   private final String lastName;
+  private final boolean admin;
   private final boolean enabled;
 
-  public User(String firstName, String lastName, boolean enabled) {
+  public User(String firstName, String lastName, boolean admin, boolean enabled) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.admin = admin;
     this.enabled = enabled;
   }
 
@@ -17,6 +19,10 @@ public class User {
 
   public String getLastName() {
     return this.lastName;
+  }
+
+  public boolean isAdmin() {
+    return this.admin;
   }
 
   public boolean isEnabled() {
